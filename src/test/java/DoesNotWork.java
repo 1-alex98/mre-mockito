343481959fff644e225b300658dda2ce43460061
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2022-03-19 12:01:59 +0100

    Simplify

--- src/test/java/DoesNotWork.java
+++ src/test/java/DoesNotWork.java
@@ -4,7 +4,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith({MockitoExtension.class})
 public class DoesNotWork extends ToBeExtended{
     @Mock
     ToBeMocked toBeMocked;
